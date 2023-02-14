0cd3c2062dabf5ec323c4825104f8e40318497f2
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-02-14 14:55:28 +0300

    Initial commit

--- lib/core/extensions/widget_extension.dart
+++ lib/core/extensions/widget_extension.dart
@@ -16,16 +16,8 @@ extension WidgetExtension on Widget {
         width: context.dynamicWidth(val),
       );
 
-  Widget dynamicMinSizePropertySpace(BuildContext context, double val) => SizedBox(
-        width: context.minSizeProperty == SizeProperty.width
-            ? context.dynamicWidth(val)
-            : 0,
-        height: context.minSizeProperty == SizeProperty.height
-            ? context.dynamicHeight(val)
-            : 0,
-      );
-
-  Widget minSizePropertySpace(BuildContext context, double val) => SizedBox(
+  Widget dynamicMinSizePropertySpace(BuildContext context, double val) =>
+      SizedBox(
         width: context.minSizeProperty == SizeProperty.width ? val : 0,
         height: context.minSizeProperty == SizeProperty.height ? val : 0,
       );
