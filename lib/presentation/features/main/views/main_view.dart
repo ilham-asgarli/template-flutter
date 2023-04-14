2a7d1a13ce6f47149fe3ccb18e444d3f7d0e1575
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-04-14 23:59:57 +0300

    Initial commit

--- lib/presentation/features/main/views/main_view.dart
+++ lib/presentation/features/main/views/main_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+class MainView extends StatelessWidget {
+  const MainView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/utils/logic/config/router/config_router.dart
+++ lib/utils/logic/config/router/config_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../../presentation/features/home/views/home_view.dart';
+import '../../../../presentation/features/main/views/main_view.dart';
 import '../../../../presentation/features/not-found-navigation/views/not_found_navigation_view.dart';
 import '../../constants/router/router_constants.dart';
 import 'interfaces/router_interface.dart';
@@ -15,7 +15,7 @@ class ConfigRouter extends RouterInterface {
     switch (settings.name) {
       case RouterConstants.home:
         return normalNavigate(
-          const HomeView(),
+          const MainView(),
           settings.name!,
         );
       default:
