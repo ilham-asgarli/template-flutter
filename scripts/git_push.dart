956d0de0d70f00610c290c07e01db1a7d2fabede
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-06-25 16:31:17 +0300

    Initial commit

--- scripts/git_push.dart
+++ scripts/git_push.dart
@@ -8,7 +8,27 @@ void main() async {
     message = 'Initial commit';
   }
 
-  await Process.run('git', ['add', '.']);
-  await Process.run('git', ['commit', '-m', message]);
-  await Process.run('git', ['push', '-u', 'origin', 'main']);
+  await Process.run('git', ['add', '.']).then((value) {
+    if (value.exitCode == 0) {
+      print('Git add successful!');
+    } else {
+      print('Git add failed. Error: ${value.stderr}');
+    }
+  });
+
+  await Process.run('git', ['commit', '-m', message]).then((value) {
+    if (value.exitCode == 0) {
+      print('Git commit successful!');
+    } else {
+      print('Git commit failed. Error: ${value.stderr}');
+    }
+  });
+
+  await Process.run('git', ['push', '-u', 'origin', 'main']).then((value) {
+    if (value.exitCode == 0) {
+      print('Git push successful!');
+    } else {
+      print('Git push failed. Error: ${value.stderr}');
+    }
+  });
 }
