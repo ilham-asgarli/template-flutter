c88f216c62dfbd92a34bcf318da96842b2a5e330
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-06-25 19:22:55 +0300

    Initial commit

--- lib/utils/logic/helpers/http/my_http_overrides.dart
+++ lib/utils/logic/helpers/http/my_http_overrides.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
       ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+          (X509Certificate cert, String host, int port) => kDebugMode; // true
   }
 }
