f3f99b0ea2a2bd55b835d407cf1fc9aa2e12b31d
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-06-25 20:02:58 +0300

    Initial commit

--- scripts/git_push.dart
+++ scripts/git_push.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 void main() async {
-  ProcessResult result;
-
   stdout.write('Enter a message: ');
   var message = stdin.readLineSync();
 
@@ -10,33 +8,36 @@ void main() async {
     message = 'Initial commit';
   }
 
-  result = await Process.run('git', ['add', '.']);
-  if (result.exitCode == 0) {
-    print(result.stdout);
-    print('Git add successful!');
-  } else {
-    print(result.stdout);
-    print(result.stderr);
-    print('Git push failed.');
-  }
+  await Process.run('git', ['add', '.']).then((value) {
+    if (value.exitCode == 0) {
+      print(value.stdout);
+      print('Git add successful!');
+    } else {
+      print(value.stdout);
+      print(value.stderr);
+      print('Git push failed.');
+    }
+  });
 
-  result = await Process.run('git', ['commit', '-m', message]);
-  if (result.exitCode == 0) {
-    print(result.stdout);
-    print('Git commit successful!');
-  } else {
-    print(result.stdout);
-    print(result.stderr);
-    print('Git push failed.');
-  }
+  await Process.run('git', ['commit', '-m', message]).then((value) {
+    if (value.exitCode == 0) {
+      print(value.stdout);
+      print('Git commit successful!');
+    } else {
+      print(value.stdout);
+      print(value.stderr);
+      print('Git push failed.');
+    }
+  });
 
-  result = await Process.run('git', ['push', '-u', 'origin', 'main']);
-  if (result.exitCode == 0) {
-    print(result.stdout);
-    print('Git push successful!');
-  } else {
-    print(result.stdout);
-    print(result.stderr);
-    print('Git push failed.');
-  }
+  await Process.run('git', ['push', '-u', 'origin', 'main']).then((value) {
+    if (value.exitCode == 0) {
+      print(value.stdout);
+      print('Git push successful!');
+    } else {
+      print(value.stdout);
+      print(value.stderr);
+      print('Git push failed.');
+    }
+  });
 }
