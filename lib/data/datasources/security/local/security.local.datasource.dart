95c4b5f9ad079429995a3bb956820eac5aa24c65
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-11-01 01:01:18 +0300

    Initial commit

--- lib/data/datasources/security/local/security.local.datasource.dart
+++ lib/data/datasources/security/local/security.local.datasource.dart
@@ -3,7 +3,6 @@ import 'package:isar/isar.dart';
 
 import '../../../../utils/constants/di/isar_constants.dart';
 import '../../../models/user_model.dart';
-import '../../../utils/exceptions/local/not_found.exception.dart';
 
 @LazySingleton()
 class SecurityLocalDataSource {
@@ -13,13 +12,9 @@ class SecurityLocalDataSource {
     @Named(IsarConstants.users) required this.isar,
   });
 
-  Future<UserModel> getUser(String id) async {
+  Future<UserModel?> getUser(String id) async {
     UserModel? userModel =
         await isar.userModels.filter().idEqualTo(id).findFirst();
-    if (userModel != null) {
-      return userModel;
-    } else {
-      throw NotFoundLocalException();
-    }
+    return userModel;
   }
 }
--- lib/data/repositories/security/local/security.local.repository.impl.dart
+++ lib/data/repositories/security/local/security.local.repository.impl.dart
@@ -3,6 +3,7 @@ import 'package:injectable/injectable.dart';
 import '../../../../domain/repositories/security/local/security.local.repository.dart';
 import '../../../datasources/security/local/security.local.datasource.dart';
 import '../../../models/user_model.dart';
+import '../../../utils/exceptions/local/not_found.exception.dart';
 
 @LazySingleton(as: SecurityLocalRepository)
 class SecurityLocalRepositoryImpl implements SecurityLocalRepository {
@@ -14,7 +15,12 @@ class SecurityLocalRepositoryImpl implements SecurityLocalRepository {
 
   @override
   Future<UserModel> getUser({required String id}) async {
-    UserModel userModel = await securityLocalDataSource.getUser(id);
-    return userModel;
+    UserModel? userModel = await securityLocalDataSource.getUser(id);
+
+    if (userModel != null) {
+      return userModel;
+    } else {
+      throw NotFoundLocalException();
+    }
   }
 }
