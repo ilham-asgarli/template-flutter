4f188cc9df3cf636f5a15aa42b904953f61f6866
ilham-asgarli <ilhamasgarli19@gmail.com>
2024-09-07 18:57:33 +0300

    fix architecture

--- lib/presentation/views/not_found_navigation_view.dart
+++ lib/presentation/views/not_found_navigation_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../utils/extensions/context_extension.dart';
-import '../widgets/have_no.dart';
+import '../widgets/app/have_no.dart';
 
 class NotFoundNavigationView extends StatelessWidget {
   const NotFoundNavigationView({super.key});
